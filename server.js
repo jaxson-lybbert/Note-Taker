b521a5e9259f532c706b36270ae349e2d5d73c99
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-06-16 21:49:12 -0400

    Added starter code and boilerplate to server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// GET Route for homepage
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
