39cf475867e20f6683cc162686550436fac5be68
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-06-16 22:38:20 -0400

    Can successfully add notes to db. Need to add route to get just one note

--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,17 @@
 const notes = require("express").Router();
+const noteData = require("../db/db.json");
 const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
 const { v4: uuidv4 } = require("uuid"); // unique id for each notes?
 
-// GET request that gets all notes from database
+// GET request that gets ALL notes from database
 notes.get("/", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
+// GET route for retrieveing just ONE note
+notes.get("/", (req, res) => {});
+
+// POST route for adding a note
 notes.post("/", (req, res) => {
   console.log(req.body);
 
@@ -18,7 +23,7 @@ notes.post("/", (req, res) => {
       text,
       uuid: uuidv4(),
     };
-    readAndAppend(newTip, "./db/db.json");
+    readAndAppend(newNote, "./db/db.json");
     res.json(`Note added successfully.`);
   } else {
     res.error("Error in adding note");
